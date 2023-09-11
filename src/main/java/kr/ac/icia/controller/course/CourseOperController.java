ab39b73243038bd611cfba3766d49edb1d05b860
mings9604 <mings9604@gmail.com>
2023-09-11 15:49:44 +0900

    23.09.11 15:49 이형민 커밋

--- src/main/java/kr/ac/icia/controller/course/CourseOperController.java
+++ src/main/java/kr/ac/icia/controller/course/CourseOperController.java
@@ -1,41 +1,98 @@
 package kr.ac.icia.controller.course;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kr.ac.icia.dto.course.CourseRegisterDto;
 import kr.ac.icia.dto.course.FilteringDto;
 import kr.ac.icia.dto.course.FilterringSearchListDto;
 import kr.ac.icia.service.course.CourseService;
-import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping({ "/course/coursereg/oper" })
+@RequestMapping(value = "/course/coursereg/oper", method = { RequestMethod.GET, RequestMethod.POST })
 public class CourseOperController {
 
 	@Autowired
 	private CourseService cSer;
-	
+
+	@Autowired
+	private HttpSession session;
+
 	// 선택한 학부 기반으로 학과 데이터 가져오기
-    @GetMapping("/filtering")
-    public List<FilteringDto> FilteringDepartment(@RequestParam String facultyId) {
-        return cSer.filtering(facultyId);
-    }
-    
-    // 필터링된 수강신청(수강신청 테이블이 아닌 과목 테이블) 리스트 가져오기
-    @GetMapping("/search")
-    public List<FilterringSearchListDto> filteringSearchList(@RequestParam Map<String, String> filterConditionMap) {
-        return cSer.filteringSearch(filterConditionMap);
-    }
+	@GetMapping("/filtering")
+	public List<FilteringDto> FilteringDepartment(@RequestParam String facultyId) {
+		return cSer.filtering(facultyId);
+	}
+
+	// 필터링된 수강신청(수강신청 테이블이 아닌 과목 테이블) 리스트 가져오기
+	@GetMapping("/search")
+	public List<FilterringSearchListDto> filteringSearchList(@RequestParam Map<String, String> filterConditionMap) {
+		return cSer.filteringSearch(filterConditionMap);
+	}
+
+	// 강의 추가
+	@PostMapping("/apply")
+	public ResponseEntity<String> addCourse(@RequestBody FilterringSearchListDto FSLDto) {
+		try {
+			List<FilterringSearchListDto> courseRegList = (List<FilterringSearchListDto>) session
+					.getAttribute("courseRegList");
+			if (courseRegList == null) {
+				courseRegList = new ArrayList<>();
+			}
+			courseRegList.add(FSLDto);
+			session.setAttribute("courseRegList", courseRegList);
+			return new ResponseEntity<>("강의가 성공적으로 추가되었습니다.", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>("강의 추가에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// 강의 취소
+	@PostMapping("/cancel")
+	public ResponseEntity<String> cancelCourse(@RequestBody FilterringSearchListDto FSLDto) {
+		try {
+			List<FilterringSearchListDto> courseRegList = (List<FilterringSearchListDto>) session
+					.getAttribute("courseRegList");
+			if (courseRegList != null) {
+				courseRegList.remove(FSLDto);
+				session.setAttribute("courseRegList", courseRegList);
+			}
+			return new ResponseEntity<>("강의가 성공적으로 취소되었습니다.", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>("강의 취소에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
+	@PostMapping("/finalapply")
+	public ResponseEntity<String> finalApply(@RequestBody List<CourseRegisterDto> CRDto) {
+		try {
+			boolean result = cSer.finalApply(CRDto);
+			if (result) {
+				return new ResponseEntity<>("수강신청 성공", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>("수강신청 실패", HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>("서버 에러", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }
--- src/main/resources/static/js/hmlee/coursereg.js
+++ src/main/resources/static/js/hmlee/coursereg.js
@@ -18,8 +18,7 @@ $(document).ready(function() {
 		});
 	});
 
-	/*------------------------------------------------------------------------------------------------------------------------------------------	*/
-
+	/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	*/
 	// SEARCH 버튼을 클릭했을 때 함수를 실행 (학부, 학과로 필터링한 데이터로 리스트 출력)
 	$("#searchButton").click(function() {
 		let facultyId = $("#facultySelect").val();
@@ -39,28 +38,118 @@ $(document).ready(function() {
 			success: function(response) {
 				console.log("Response:", response);
 				// 테이블에 데이터를 추가
-				let table = $("#courseTableBody");
+				let table = $("#courseRegTableBody");
 				table.empty(); // 기존 내용을 지움
 
 				// 데이터가 없을 경우 출력
 				if (response.length === 0) {
-					table.append('<tr class="first last"><td class="NO_RESULT first last" colspan="8">검색된 데이터 없음.</td></tr>');
+					table.append('<tr class="first last"><td class="NO_RESULT first last" colspan="8">검색된 데이터가 없습니다.</td></tr>');
 				} else {
 					// forEach를 사용하여 각 아이템에 대한 처리 수행
 					//실제 필터링된 데이터 기반으로 리스트 생성 부분
-					
+
 					response.forEach(function(item) {
 						console.log(item);
-						table.append('<tr><td><button class="applyButton">신청</button></td><td>' + item.grade + "/" + item.semester + '</td><td>' + item.course_division + '</td><td>' + item.course_id + '</td><td>' + item.credit + '</td><td>' + item.course_name + '</td><td>' + item.pf_name + '</td><td>' + item.course_time + '</td></tr>');
+						table.append('<tr data-id="' + item.course_id + '"><td><button class="applyButton" type="button">신청</button></td><td>' + item.grade + "/" + item.semester + '</td><td>' + item.course_division + '</td><td>' + item.course_id + '</td><td>' + item.credit + '</td><td>' + item.course_name + '</td><td>' + item.pf_name + '</td><td>' + item.course_time + '</td></tr>');
 					});
 				}
 			}
 		});
 	};
-	
+
 	// 초기에 함수를 실행
 	updateTable(null, null); // 빈 필터 기준으로 모든 데이터 가져오기
 
-	/*------------------------------------------------------------------------------------------------------------------------------------------	*/
+	/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	*/
+
+	// '수강신청현황' 테이블에 행을 추가하는 함수
+	const addToCourseRegStatusTable = function(rowData) {
+		let courseRegStatusTable = $("#courseRegStatusTableBody");
+		courseRegStatusTable.append('<tr><td><button class="cancelButton" type="button">취소</button></td><td>' + rowData.grade_semester + '</td><td>' + rowData.course_division + '</td><td>' + rowData.course_id + '</td><td>' + rowData.credit + '</td><td>' + rowData.course_name + '</td><td>' + rowData.pf_name + '</td><td>' + rowData.course_time + '</td></tr>');
+		updateNoDataMessage(courseRegStatusTable);
+	};
+
+	// 데이터가 없을 때, '데이터 없음.' 텍스트를 업데이트하는 함수
+	const updateNoDataMessage = function(tableElement) {
+		if (tableElement.find('tr:not([data-hidden=true]):visible').length === 0) {
+			tableElement.append('<tr class="first last"><td class="NO_RESULT first last" colspan="8">데이터가 없습니다.</td></tr>');
+		} else {
+			tableElement.find('.NO_RESULT').parent().remove();
+		}
+	};
+
+	// '신청' 버튼을 클릭하면 실행되는 함수
+	$(document).on('click', '.applyButton', function() {
+		const confirmResult = confirm("이 강의를 신청하시겠습니까?");
+		if (confirmResult) {
+			const rowElement = $(this).closest('tr');
+			const rowData = extractRowData(rowElement);
+			rowElement.attr("data-hidden", "true"); // 행을 숨겨진 것으로 표시
+			rowElement.hide(); // 행 숨기기
+			$.ajax({
+				url: '/course/coursereg/oper/apply',
+				method: 'POST',
+				data: JSON.stringify(rowData),
+				contentType: 'application/json',
+				success: function(response) {
+					addToCourseRegStatusTable(rowData);
+				}
+			});
+		}
+	});
+
+	// '취소' 버튼을 클릭하면 실행되는 함수
+	$(document).on('click', '.cancelButton', function() {
+		const confirmResult = confirm("이 강의를 취소하시겠습니까?");
+		if (confirmResult) {
+			const rowElement = $(this).closest('tr');
+			const rowData = extractRowData(rowElement);
+			rowElement.remove(); // 행 제거
+			$(`tr[data-id=${rowData.course_id}]`).attr('data-hidden', 'false').show(); // '수강 신청' 테이블에서 해당 ID의 행을 보여줌
+			$.ajax({
+				url: '/course/coursereg/oper/cancel',
+				method: 'POST',
+				data: JSON.stringify(rowData),
+				contentType: 'application/json',
+				success: function(response) {
+					updateNoDataMessage($("#courseRegStatusTableBody"));
+				}
+			});
+		}
+	});
+
+	// 테이블의 행에서 데이터를 추출하는 함수입니다.
+	const extractRowData = function(parentRow) {
+		const rowData = {};
+		rowData.grade_semester = parentRow.find('td').eq(1).text();  // "학년/학기"
+		rowData.course_division = parentRow.find('td').eq(2).text(); // "구분"
+		rowData.course_id = parentRow.find('td').eq(3).text();       // "강의 번호"
+		rowData.credit = parentRow.find('td').eq(4).text();          // "학점"
+		rowData.course_name = parentRow.find('td').eq(5).text();     // "과목명"
+		rowData.pf_name = parentRow.find('td').eq(6).text();         // "교수명"
+		rowData.course_time = parentRow.find('td').eq(7).text();     // "강의 시간"
+		return rowData;
+	};
+
+	/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	*/
+
+	document.getElementById('courseRegisterButton').addEventListener('click', function() {
+		// 현재 세션의 ArrayList를 가져옵니다 (어떻게 구현했는지에 따라 다름)
+		const finalCourseList = fetchSessionArrayList();
+
+		// AJAX를 통해 서버로 ArrayList를 전송합니다
+		$.ajax({
+			url: "'/course/coursereg/oper/finalapply",
+			type: "POST",
+			contentType: "application/json",
+			data: JSON.stringify(finalCourseList),
+			success: function(response) {
+				alert(response);
+			},
+			error: function(err) {
+				alert("error: " + err);
+			}
+		});
+	});
 
 });
